aa60d1cbd57ebdd8e1dbae9d935bcade13ab675c
Izugarri <57817632+Izugarri@users.noreply.github.com>
2021-08-25 12:01:08 +0200

    Flashdrive

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname+'/'));
+
+console.log('escuchando en el puerto 8080 y 80');
+app.listen(8080);
+app.listen(80);
