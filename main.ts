db5554263c1b0ce46795e1c984da5cdf5fcb2120
Izugarri <57817632+Izugarri@users.noreply.github.com>
2026-03-26 09:53:23 +0100

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,9 @@
+import { serveDir } from "jsr:@std/http/file-server";
+
+Deno.serve((req) => {
+  return serveDir(req, {
+    fsRoot: "./", // Carpeta que contiene los archivos
+    showDirListing: true,
+    pretty url: true,
+  });
+});
